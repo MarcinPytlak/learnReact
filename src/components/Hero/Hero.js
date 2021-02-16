9a14b8968871b452efca85c371813a046b9cd4dd
Marcin Pytlak <marcin.pytlak@o2.pl>
2021-02-16 18:28:16 +0100

    First commit + first task finish

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+const Hero = () => (
+    <header className={styles.component}>
+        <h2 className={styles.title}>Lista rzeczy</h2>
+        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"></img>
+    </header>
+
+);
+
+export default Hero;
